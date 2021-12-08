ebbd3caf2312fe128b05d2429ffd85b1f06b8da0
KyrylHodlevskiy <kn2004k@gmail.com>
2021-12-08 21:10:45 +0200

    Добавьте файлы проекта.

---
+++ UnitTest8.1.4/UnitTest8.1.4.cpp
@@ -0,0 +1,37 @@
+﻿#include "pch.h" 
+
+#include "CppUnitTest.h" 
+
+#include "C:\Users\Pavilion\Desktop\Універ\Алгоритмізація та програмування\Лабораторна 8.1\4 спосіб\lab8.1.4\lab8.1.4\lab8.1.4.cpp"     
+
+
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+
+
+namespace UnitTest1
+
+{
+
+    TEST_CLASS(UnitTest1)
+
+    {
+
+    public:
+
+
+
+        TEST_METHOD(TestMethod1)
+
+        {
+
+             int R = Count("TESTQQQ","Q");
+
+            Assert::AreEqual(3, R);
+
+        }
+
+    };
+
+}
---
+++ lab8.1.4/lab8.1.4.cpp
@@ -0,0 +1,51 @@
+﻿#include <iostream> 
+
+#include <string> 
+
+using namespace std;
+
+
+
+int Count(const string s, const string substr)
+{
+    size_t pos = 0;
+    int k = 0;
+    while ((pos = s.find(substr, pos)) != -1)
+    {
+        k++;
+        pos++;
+    }
+    return k;
+}
+
+string Replace(string& str)
+
+{
+
+    while (str.find("abc") != -1)
+
+        str = str.replace(str.find("abc"), 3, "**");
+
+
+    return str;
+
+}
+
+int main()
+
+{
+
+    string str;
+    string s = "abc";
+
+    cout << "Enter string:" << endl;
+
+    getline(cin, str);
+
+    cout << Count(str, s) << endl;
+
+    cout << Replace(str) << endl;
+
+    cout << endl;
+
+}
